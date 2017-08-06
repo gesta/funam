d5dda194094c566824da98f13bcd0a1600486f44
Gerasim Stanchev <gero@tutuf.com>
2017-08-06 19:44:43 +0300

    Link a GenServer to Funam's app

--- lib/server.ex
+++ lib/server.ex
@@ -1,4 +1,24 @@
 defmodule Funam.Server do
   use GenServer
 
+  def start_link do
+    GenServer.start_link(__MODULE__, [], [name: {:global, __MODULE__}])
+  end
+
+  def translate(phrase) do
+    GenServer.call({:global, __MODULE__}, {:translate, phrase})
+  end
+
+
+  def init([]) do
+    {:ok, []}
+  end
+
+  def handle_call({:translate, phrase}, _from, _wut) do
+    {:reply, translation_query(phrase), []}
+  end
+
+  def translation_query(phrase) do
+    "https://glosbe.com/gapi/translate?from=eng&dest=bg&format=json&phrase=#{phrase}"
+  end
 end
