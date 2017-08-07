d7ec638100ed728c7e68175c5e98203b63325a85
Gerasim Stanchev <gero@tutuf.com>
2017-08-07 22:57:29 +0300

    Add HTTPotion & Poison as dependencies

--- mix.exs
+++ mix.exs
@@ -4,18 +4,19 @@ defmodule Funam.Mixfile do
   def project do
     [app: :funam,
      version: "0.0.1",
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     elixir: "~> 1.4",
      deps: deps()]
   end
 
   def application do
-    [applications: [:logger],
-      mod: {Funam, []}]
+    [mod: {Funam, []},
+     applications: [:logger, :httpotion, :poison]]
   end
 
   defp deps do
-    []
+    [
+      {:httpotion, "~> 3.0.2"},
+      {:poison, "~> 3.1.0"},
+    ]
   end
 end
