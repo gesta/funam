f94d84913b5709b3daa0e99168798977ef03c6c8
Gerasim Stanchev <gero@tutuf.com>
2017-08-24 02:58:23 +0300

    Set the app to start pool of supervisors and workers

--- lib/funam.ex
+++ lib/funam.ex
@@ -1,9 +1,30 @@
 defmodule Funam do
-  use Application
   require Logger
-  require HTTPotion
 
-  def start(type, _args) do
+  use Application
+
+  @timeout 5000
+
+  def start(type, _args \\ []) do
+    pools_config =
+      [
+        [name: "Pool1",
+         mfa: {Worker, :start_link, []},
+         size: 3,
+         max_overflow: 1
+        ],
+        [name: "Pool2",
+         mfa: {Worker, :start_link, []},
+         size: 3,
+         max_overflow: 0
+        ],
+        [name: "Pool3",
+         mfa: {Worker, :start_link, []},
+         size: 3,
+         max_overflow: 0
+        ],
+      ]
+
     case type do
       :normal ->
         Logger.info("Application starts on #{node()}")
@@ -15,6 +36,22 @@ defmodule Funam do
         Logger.info("#{old_node} fails over to #{node()}")
     end
 
-    Funam.Supervisor.start_link(:ok)
+    start_pools(pools_config)
+  end
+
+  def start_pools(pools_config) do
+    Funam.Supervisor.start_link(pools_config)
+  end
+
+  def checkout(pool_name, block \\ true, timeout \\ @timeout) do
+    Funam.Server.checkout(pool_name, block, timeout)
+  end
+
+  def checkin(pool_name, worker_pid) do
+    Funam.Server.checkin(pool_name, worker_pid)
+  end
+
+  def status(pool_name) do
+    Funam.Server.status(pool_name)
   end
 end
