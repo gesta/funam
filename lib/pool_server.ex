fa7ad55ad64085878e2f2a82a01a77085032ecef
Gerasim Stanchev <gero@tutuf.com>
2017-08-24 03:16:07 +0300

    Define inner state module for convenience

--- lib/pool_server.ex
+++ lib/pool_server.ex
@@ -2,6 +2,20 @@ defmodule Funam.PoolServer do
   use GenServer
   import Supervisor.Spec
 
+  defmodule State do
+    defstruct pool_sup: nil,
+    worker_sup: nil,
+    monitors: nil,
+    monitors: nil,
+    size: nil,
+    workers: nil,
+    name: nil,
+    mfa: nil,
+    waiting: nil,
+    overflow: nil,
+    max_overflow: nil
+  end
+
   def start_link(pool_sup, pool_config) do
     GenServer.start_link(__MODULE__, [pool_sup, pool_config], name: name(pool_config[:name]))
   end
