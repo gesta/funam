a4ebae2800e8a9b08572218a2b767682753fe35f
Gerasim Stanchev <gero@tutuf.com>
2017-08-24 03:19:29 +0300

    Define a supervisor for a worker

---
+++ lib/worker_supervisor.ex
@@ -0,0 +1,21 @@
+defmodule Funam.WorkerSupervisor do
+  use Supervisor
+
+  def start_link(pool_server, {_,_,_} = mfa) do
+    Supervisor.start_link(__MODULE__, [pool_server, mfa])
+  end
+
+  def init([pool_server, {m,f,a}]) do
+    Process.link(pool_server)
+    worker_opts = [restart: :temporary,
+                   shutdown: 5000,
+                   function: f]
+
+    children = [worker(m, a, worker_opts)]
+    opts     = [strategy:    :simple_one_for_one,
+                max_restart: 5,
+                max_time:    3600]
+
+    supervise(children, opts)
+  end
+end
