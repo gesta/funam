38d3ff79f01ee19df5c12f01566ee5ce78816a7c
Gerasim Stanchev <gero@tutuf.com>
2017-08-24 03:00:46 +0300

    Define the Funam.Server API functions

--- lib/server.ex
+++ lib/server.ex
@@ -1,22 +1,24 @@
 defmodule Funam.Server do
+  use GenServer
+  import Supervisor.Spec
   require Logger
 
-  def do_it(phrase) do
-    Funam.Server.start_link()
-    Funam.Server.translate(phrase)
+
+  def start_link(pools_config) do
+    GenServer.start_link(__MODULE__, pools_config, name: __MODULE__)
   end
 
-  def translate_phrase(url, nodes \\ Node.list()) do
-    Funam.Worker.start(translation_query(url))
+  def checkout(pool_name, block, timeout) do
+    Funam.PoolServer.checkout(pool_name, block, timeout)
   end
 
-  defp do_help do
-    IO.puts "Summat went wrong"
+  def checkin(pool_name, worker_pid) do
+    Funam.PoolServer.checkin(pool_name, worker_pid)
   end
 
- def start_link do
-   GenServer.start_link(__MODULE__, [], [name: {:global, __MODULE__}])
- end
+  def status(pool_name) do
+    Funam.PoolServer.status(pool_name)
+  end
 
  def translate(phrase) do
    GenServer.call({:global, __MODULE__}, {:translate, phrase})
