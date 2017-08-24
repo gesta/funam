1df980ecb080c5d7cc8242c1cf20a9571120a2e4
Gerasim Stanchev <gero@tutuf.com>
2017-08-24 03:15:37 +0300

    Define pool server's API

---
+++ lib/pool_server.ex
@@ -0,0 +1,20 @@
+defmodule Funam.PoolServer do
+  use GenServer
+  import Supervisor.Spec
+
+  def start_link(pool_sup, pool_config) do
+    GenServer.start_link(__MODULE__, [pool_sup, pool_config], name: name(pool_config[:name]))
+  end
+
+  def checkout(pool_name, block, timeout) do
+    GenServer.call(name(pool_name), {:checkout, block}, timeout)
+  end
+
+  def checkin(pool_name, worker_pid) do
+    GenServer.cast(name(pool_name), {:checkin, worker_pid})
+  end
+
+  def status(pool_name) do
+    GenServer.call(name(pool_name), :status)
+  end
+end
