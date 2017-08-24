9c65453aff9ab266bfa1bf18dba1a07da8a407ab
Gerasim Stanchev <gero@tutuf.com>
2017-08-24 03:17:11 +0300

    Declare pool server's callbacks

--- lib/pool_server.ex
+++ lib/pool_server.ex
@@ -31,4 +31,141 @@ defmodule Funam.PoolServer do
   def status(pool_name) do
     GenServer.call(name(pool_name), :status)
   end
+
+
+  def init([pool_sup, pool_config]) when is_pid(pool_sup) do
+    Process.flag(:trap_exit, true)
+    monitors = :ets.new(:monitors, [:private])
+    waiting  = :queue.new
+    state    = %State{pool_sup: pool_sup, monitors: monitors, waiting: waiting, overflow: 0}
+
+    init(pool_config, state)
+  end
+
+  def init([{:name, name}|rest], state) do
+    init(rest,  %{state | name: name})
+  end
+
+  def init([{:mfa, mfa}|rest], state) do
+    init(rest,  %{state | mfa: mfa})
+  end
+
+  def init([{:size, size}|rest], state) do
+    init(rest, %{state | size: size})
+  end
+
+  def init([{:max_overflow, max_overflow}|rest], state) do
+    init(rest, %{state | max_overflow: max_overflow})
+  end
+
+  def init([], state) do
+    send(self, :start_worker_supervisor)
+    {:ok, state}
+  end
+
+  def init([_|rest], state) do
+    init(rest, state)
+  end
+
+  def handle_call({:checkout, block}, {from_pid, _ref} = from, state) do
+    %{worker_sup:   worker_sup,
+      workers:      workers,
+      monitors:     monitors,
+      waiting:      waiting,
+      overflow:     overflow,
+      max_overflow: max_overflow} = state
+
+    case workers do
+      [worker|rest] ->
+        ref = Process.monitor(from_pid)
+        true = :ets.insert(monitors, {worker, ref})
+        {:reply, worker, %{state | workers: rest}}
+
+      [] when max_overflow > 0 and overflow < max_overflow ->
+        {worker, ref} = new_worker(worker_sup, from_pid)
+        true = :ets.insert(monitors, {worker, ref})
+        {:reply, worker, %{state | overflow: overflow+1}}
+
+      [] when block == true ->
+        ref = Process.monitor(from_pid)
+        waiting = :queue.in({from, ref}, waiting)
+        {:noreply, %{state | waiting: waiting}, :infinity}
+
+      [] ->
+        {:reply, :full, state};
+    end
+  end
+
+  def handle_call(:status, _from, %{workers: workers, monitors: monitors} = state) do
+    {:reply, {state_name(state), length(workers), :ets.info(monitors, :size)}, state}
+  end
+
+  def handle_call(_msg, _from, state) do
+    {:reply, {:error, :invalid_message}, :ok, state}
+  end
+
+  def handle_cast({:checkin, worker}, %{monitors: monitors} = state) do
+    case :ets.lookup(monitors, worker) do
+      [{pid, ref}] ->
+        true = Process.demonitor(ref)
+        true = :ets.delete(monitors, pid)
+        new_state = handle_checkin(pid, state)
+        {:noreply, new_state}
+
+      [] ->
+        {:noreply, state}
+    end
+  end
+
+  def handle_info(:start_worker_supervisor, state = %{pool_sup: pool_sup, name: name, mfa: mfa, size: size}) do
+    {:ok, worker_sup} = Supervisor.start_child(pool_sup, supervisor_spec(name, mfa))
+    workers = prepopulate(size, worker_sup)
+    {:noreply, %{state | worker_sup: worker_sup, workers: workers}}
+  end
+
+  def handle_info({:DOWN, ref, _, _, _}, state = %{monitors: monitors, workers: workers}) do
+    case :ets.match(monitors, {:"$1", ref}) do
+      [[pid]] ->
+        true = :ets.delete(monitors, pid)
+        new_state = %{state | workers: [pid|workers]}
+        {:noreply, new_state}
+
+      [[]] ->
+        {:noreply, state}
+    end
+  end
+
+  def handle_info({:EXIT, worker_sup, reason}, state = %{worker_sup: worker_sup}) do
+    {:stop, reason, state}
+  end
+
+  def handle_info({:EXIT, pid, _reason}, state = %{monitors: monitors, workers: workers, worker_sup: worker_sup}) do
+    case :ets.lookup(monitors, pid) do
+      [{pid, ref}] ->
+        true = Process.demonitor(ref)
+        true = :ets.delete(monitors, pid)
+        new_state = handle_worker_exit(pid, state)
+        {:noreply, new_state}
+
+      [] ->
+        # NOTE: Worker crashed, no monitor
+        case Enum.member?(workers, pid) do
+          true ->
+            remaining_workers = workers |> Enum.reject(fn(p) -> p == pid end)
+            new_state = %{state | workers: [new_worker(worker_sup)|remaining_workers]}
+            {:noreply, new_state}
+
+            false ->
+              {:noreply, state}
+        end
+    end
+  end
+
+  def handle_info(_info, state) do
+    {:noreply, state}
+  end
+
+  def terminate(_reason, _state) do
+    :ok
+  end
 end
